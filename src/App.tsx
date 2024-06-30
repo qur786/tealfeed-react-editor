20248743cc0af0e01f68240b4ceda365231faaaa
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-06-30 22:35:37 +0530

    refactor: remove initial text

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,6 @@ import { CodeEditor } from "./compoents/CodeEditor";
 import "./App.css";
 
 export function App(): JSX.Element {
-  const initialText = `
-import React from "react";
-import ReactDOM from "react-dom";
-
-function App() {
-  return (
-    <h1>Hello world</h1>
-  );
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
-`;
   return (
     <div className="main-container">
       <p className="heading">Custom Editor</p>
@@ -23,7 +11,6 @@ ReactDOM.render(<App />, document.getElementById("root"));
           height: 400,
           border: "1px solid black",
         }}
-        initialText={initialText}
       />
     </div>
   );
