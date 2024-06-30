733998bd44cf6670a9e6f9f309c65b219318074b
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-06-30 22:13:54 +0530

    refactor: added heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { CodeEditor } from "./compoents/CodeEditor";
+import "./App.css";
 
 export function App(): JSX.Element {
   return (
-    <CodeEditor
-      style={{
-        width: "100%",
-        height: 400,
-        border: "1px solid black",
-      }}
-    />
+    <div className="main-container">
+      <p className="heading">Custom Editor</p>
+      <CodeEditor
+        style={{
+          width: "100%",
+          height: 400,
+          border: "1px solid black",
+        }}
+      />
+    </div>
   );
 }
